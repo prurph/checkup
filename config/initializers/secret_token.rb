56fa51bbbcb6d29aca631e871f714a90cb059bf0
Prescott Murphy <prurph@gmail.com>
2014-02-28 22:36:13 -0500

    Initial commit with db created and blank Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CheckUp::Application.config.secret_key_base = 'a99abcc89884694eef3ef823e8899d25f6d90003eabe20d07d522843fcdb2d340c12facdad7357265ff97c400fb01d3cd6ebedbf2f31f8eb2ed30970fd451c04'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CheckUp::Application.config.session_store :cookie_store, key: '_check_up_session'
