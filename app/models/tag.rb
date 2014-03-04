5c7cae0b93a66fe597d2f19ad77449c473dad1c6
Praggya Rustagi Barretto <praggya@gmail.com>
2014-03-03 20:13:05 -0500

    updated tag spec to reject dupe active tag names and allow dupe inactive tag names

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,8 @@ class Tag < ActiveRecord::Base
   belongs_to :category, touch: true
   has_many :events
 
-  validates :name, uniqueness: { scope: [:category, :active] }
+  validates :name, uniqueness: { scope: [:category, :active] },
+    unless: Proc.new { |category| !category.active }
 
   delegate :color, to: :category
 end
